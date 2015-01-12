b33ed65138268bea24fc3236fe9ba37d8ec14247
Matthew Power <matthew.power@hogarthww.com>
2015-01-12 00:46:26 +0000

    Actually do the work in the function.

--- src/crypto/set_1/challenge_1.py
+++ src/crypto/set_1/challenge_1.py
@@ -2,4 +2,6 @@ from base64 import b64encode
 
 
 def convert_hex_to_base64(hex_string):
-    return b64encode(hex_string)
+    """ Given a hex string, return the base64 encoded bytes. """
+    decoded_hex = bytes.fromhex(hex_string)
+    return b64encode(decoded_hex)
